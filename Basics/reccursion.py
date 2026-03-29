9f6a0f49e0fc4074e8199b8e3d42cf760f8fa142
ztarikul <ztarikulislam@gmail.com>
2026-03-29 17:52:26 +0600

    recurrsion add exponential

--- Basics/reccursion.py
+++ Basics/reccursion.py
@@ -1,8 +1,22 @@
-def factorial(n):
-    if n == 0 or n == 1:
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+
+
+# print(factorial(5))
+
+
+#exponential number
+
+def power(base, exponent):
+    if exponent == 0:
         return 1
     else:
-        return n * factorial(n-1)
+        return base * power(base, exponent-1) 
+        #background: 2 * (2 * 2) => 2 * (2 * 1) => return 1
+    
 
 
-print(factorial(5))
+print(power(2,3))
