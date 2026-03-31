7a18c795401fec572208556ab65e5fd2f7fd1605
ztarikul <ztarikulislam@gmail.com>
2026-04-01 00:28:56 +0600

    C++ intro

---
+++ Basics/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "You entered: " << n << endl;
+    return 0;
+}
+
+// clang++ hello.cpp -o hello
+// ./hello
